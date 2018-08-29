a233b484f03597eacd959adf14906f5d5dee02df
MininMobile <miningamer@gmail.com>
2018-08-29 22:57:03 +0100

    add menu

--- src/cide.js
+++ src/cide.js
@@ -1,16 +1,17 @@
 const remote = require("electron").remote;
+const w = remote.getCurrentWindow();
 const application = { name: "CIDE" }; // replace with require("./lib/application") soon pls
 const components = {
 	title: document.getElementById("title"),
+	altmenu: document.getElementById("altmenu"),
 	minimizeApp: document.getElementById("action-minimize"),
 	maximizeApp: document.getElementById("action-maximize"),
 	closeApp: document.getElementById("action-close")
 };
 
 {
+	components.minimizeApp.onclick = w.minimize;
 	components.maximizeApp.onclick = () => {
-		let w = remote.getCurrentWindow();
-
 		if (w.isMaximized()) {
 			components.maximizeApp.classList.add("button-maximize");
 			components.maximizeApp.classList.remove("button-maximized");
@@ -24,9 +25,30 @@ const components = {
 	components.closeApp.onclick = window.close;
 }
 
+generateMenu({
+	File: undefined,
+	Edit: undefined,
+	View: undefined,
+	Tools: undefined,
+	Help: undefined,
+});
+
 setTitle(`Welcome - ${application.name}`);
 
 function setTitle(title) {
 	document.title = title;
 	components.title.innerText = title;
+}
+
+function generateMenu(menu) {
+	components.altmenu.innerHTML = "";
+
+	Object.keys(menu).forEach((item) => {
+		let button = document.createElement("div");
+		button.classList.add("menu-item", "button-small");
+		button.innerText = item;
+		button.onclick = menu[item];
+
+		components.altmenu.appendChild(button);
+	});
 }
